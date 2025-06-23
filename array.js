45fa7c30305f9ec113573b30054a541893155a0c
RANJAN655 <ranjankumar976666@gmail.com>
2025-06-24 08:16:02 +0900

    add array.js

---
+++ array.js
@@ -0,0 +1,117 @@
+const rk = [1,2,3,4,5]
+// const rk2 =[7,'ranjan',9,0, true, 'das', false]
+// console.log(rk);
+// console.log(rk2);
+// const rk3 = new Array(2,9,3,6);
+// console.log(rk3)
+// const rk4 = new Array('kunu',9,89,'true')
+// console.log(typeof rk4)
+// console.log(rk.push(90))
+// rk.pop()
+// const kunu= rk.join()
+// console.log(kunu);
+
+ 
+
+
+// console.log(rk);
+
+// rk.unshift(101)
+// rk.shift()
+// rk.shift()
+
+// console.log(rk);
+
+// console.log(rk.includes(89));
+// console.log(rk.indexOf(5));
+// rk.push(2)
+// rk.push(5)
+// console.log(rk);
+
+// console.log(rk.lastIndexOf(5));
+// console.log(rk.findIndex(3,5));
+
+
+// console.log('a',rk);
+
+// const diff = rk.slice(1,3)
+// console.log('b',rk);
+
+// console.log(diff);
+
+// const diff2 =rk.splice(1,3)
+// console.log(rk);
+// console.log(diff2);
+
+// const marbal_hero = ['spider man ','iron man', 'thor']
+// const dc_hero = ['super man', 'batman','flash']
+// console.log(marbal_hero)
+// console.log(dc_hero);
+
+// marbal_hero.push(dc_hero)
+// console.log(dc_hero);
+
+// console.log(marbal_hero)
+
+// let kune = marbal_hero.concat(dc_hero)
+// console.log(kune);
+
+        //    spread operator
+
+// const kunu2 = [...marbal_hero,...dc_hero]
+// console.log(kunu2);
+
+const a=80
+// a=44 //not possible
+console.log(a);
+// console.log(b);
+
+// {
+// let c=90
+// let c =55  not possible
+// }
+// console.log(c);
+// console.log(d);
+
+        //       flat operator
+
+let another = [1,2,[9,6,8],3,4,[0,8,[7,7,7,8,8,9,9,6,6],2,1,4],44,6]
+let another_array = another .flat(2)
+console.log(another_array);
+console.log(Array.isArray("rakes"));
+console.log(Array.from("rakes"));
+console.log(Array.from({
+        name: 'ranjan',
+        place: 'jagannath'
+}));
+
+let score1 = 100
+let score2 = 100
+let score3 = 100
+let score4 = 100
+console.log(Array.of(score1,score2,score3,score4));
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
